7cc44eea97e32439f90727677908e3c93ca1bd17
tyousk <tyou22211@gmail.com>
2021-10-25 12:05:10 +0900

    投稿バリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
 	belongs_to :Prefecture
 	belongs_to :category
   has_one_attached :image
+
+	validates :cooking_name, :impression, :price, :opening, :closing, :city, :address, presence: true
+	validates :category_id, :prefecture_id, numericality: { other_than: 1 }
+
 end
