f7ac11c4ec3c50b3aa690609c550093f8a8cc2dc
tyousk <tyou22211@gmail.com>
2021-10-28 09:54:22 +0900

    userモデルのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+  validates :nickname
+  validates :birth_day
+
+  with_options format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ } do
+    validates :password
+  end
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  validates :gender_id, numericality: { other_than: 1 }
+end
 
 end
